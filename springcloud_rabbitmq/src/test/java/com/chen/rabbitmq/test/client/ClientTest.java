1c79dd561a4efb80b2cf089b5418437901d851ce
cjg260508119 <260508119@qq.com>
2023-04-17 11:10:31 +0800

    rabbitmq消息中间件

---
+++ springcloud_rabbitmq/src/test/java/com/chen/rabbitmq/test/client/ClientTest.java
@@ -0,0 +1,18 @@
+package com.chen.rabbitmq.test.client;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @ClassName:ClientTest
+ * @Auther: 26050
+ * @Description: 使用原生客户端连接rabbitmq
+ * @Date: 2023/4/17 10:29
+ * @Version: v1.0
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClientTest {
+
+}
